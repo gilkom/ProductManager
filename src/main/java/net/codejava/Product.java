08f298d931241ac05fa80d69ed3a0feb9a0e8249
gilkom <gilek06@op.pl>
2020-05-04 14:42:43 +0200

    Adding ProductRepository interface and Product Service class

--- src/main/java/net/codejava/Product.java
+++ src/main/java/net/codejava/Product.java
@@ -27,7 +27,7 @@ public class Product {
 	public String getName() {
 		return name;
 	}
-	public void setName(String brand) {
+	public void setName(String name) {
 		this.name = name;
 	}
 	public String getBrand() {
