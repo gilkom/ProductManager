e170e0228287d1ee7821239e68e679d3f26c7e67
gilkom <gilek06@op.pl>
2020-05-05 16:36:33 +0200

    adding edit_product.html and controller

--- src/main/java/net/codejava/AppController.java
+++ src/main/java/net/codejava/AppController.java
@@ -5,7 +5,11 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class AppController {
@@ -26,5 +30,19 @@ public class AppController {
 		model.addAttribute("product", product);
 		
 		return "new_product";
+	}
+	@RequestMapping(value = "/save", method = RequestMethod.POST)
+	public String saveProduct(@ModelAttribute("product") Product product) {
+		service.save(product);
+		
+		return "redirect:/";
+	}
+	@RequestMapping("/edit/{id}")
+	public ModelAndView showEditProductPage(@PathVariable(name = "id") int id) {
+		ModelAndView mav = new ModelAndView("edit_product");
+		Product product = service.get(id);
+		mav.addObject("product", product);
+		
+		return mav;
 	}
 }
